0568a2e10a6699b3e814ae68cf313d5cfd717b37
daeun <leede0418@likelion.org>
2020-05-14 02:45:06 +0900

    refactor: Restructure source code with Lombok

--- src/main/java/com/jojoldu/book/springboot/web/HelloController.java
+++ src/main/java/com/jojoldu/book/springboot/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.jojoldu.book.springboot.web;
 
+import com.jojoldu.book.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController                             // Controller --> JSON 반환하는 Controller 변환
@@ -9,5 +11,11 @@ public class HelloController {
     @GetMapping("/hello")                   // GET(HTTP Method) 요청 받을 수 있는 API 생성
     public String hello() {
         return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name,
+                                     @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
     }
 }
